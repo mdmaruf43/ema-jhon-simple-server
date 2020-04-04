8ade967ed0bf62fca5e4fc2e7129fb08220b99cc
mdmaruf43 <marufahmedcse76@gmail.com>
2020-04-04 06:48:12 +0600

    new api create for react integration

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/products', (req, res) => {
     client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
         const collection = client.db("onlineStore").collection("products");
-        collection.find().limit(10).toArray((err, documents) => {
+        collection.find().toArray((err, documents) => {
             if(err){
                 console.log(err)
                 res.status(500).send({message: err});
@@ -30,6 +30,48 @@ app.get('/products', (req, res) => {
     });
 });
 
+app.get('/product/:key', (req, res) => {
+    const key = req.params.key;
+    // const name = users[id];
+    // console.log(requset.query.sort);
+    // response.send({id, name});
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.find({key}).toArray((err, documents) => {
+            if(err){
+                console.log(err)
+                res.status(500).send({message: err});
+            }
+            else{
+                res.send(documents[0]);
+            }
+        });
+        client.close();
+    });
+})
+
+
+app.post('/getProductByKey', (req, res) => {
+    const key = req.params.key;
+    const productKeys = req.body;
+    //console.log(productKeys);
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.find({key: {$in: productKeys}}).toArray((err, documents) => {
+            if(err){
+                console.log(err)
+                res.status(500).send({message: err});
+            }
+            else{
+                res.send(documents);
+            }
+        });
+        client.close();
+    });
+})
+
 
 app.post('/addProduct', (req, res) => {
     //save to database
@@ -37,7 +79,7 @@ app.post('/addProduct', (req, res) => {
     client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
         const collection = client.db("onlineStore").collection("products");
-        collection.insertOne(product, (err, result) => {
+        collection.insert(product, (err, result) => {
             if(err){
                 console.log(err)
                 res.status(500).send({message: err});
@@ -50,13 +92,26 @@ app.post('/addProduct', (req, res) => {
     });
 })
 
-
-app.get('/user/:id', (requset, response) => {
-    const id = requset.params.id;
-    const name = users[id];
-    console.log(requset.query.sort);
-    response.send({id, name});
+app.post('/placeOrder', (req, res) => {
+    const orderDetails = req.body;
+    orderDetails.orderTime = new Date();
+    //console.log(orderDetails);
+    client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("orders");
+        collection.insertOne(orderDetails, (err, result) => {
+            if(err){
+                console.log(err)
+                res.status(500).send({message: err});
+            }
+            else{
+                res.send(result.ops[0]);
+            }
+        });
+        client.close();
+    });
 })
+
 
 const port = process.env.PORT || 4200
 app.listen(port, () => console.log('Listing to port 3000'));
